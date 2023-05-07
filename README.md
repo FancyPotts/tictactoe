138d930b83f30a52fdcb322451859164b8df3d0b
pottsaf <aaronfpotts@gmail.com>
2023-05-06 18:43:55 -0600

    Add new function that displays array and upon clicking, display circle or cross

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # tictactoe
+This is a deep dive into encapsulation, privatizing objects, and exploring the different ways of how functions can interact with each other. Use JS to build the game application and make it easy to modify
+
 -[x] Create repo
 -[x] Create files
 -[ ] Build the following:
     - [ ] gameBoard (Module)
+        - [ ] Generate board
+        - [ ] Use nested event listeners
     - [ ] displayController (Module)
+        - [ ] Determine player turn.
     - [ ] players (factory)
+        - [ ] function for player action
 - [ ] Display gameBoard
-- [ ] Build functions to let players mark specific parts of the gameBoard
+- [ ] Build-in player function marking grid using the nested event listeners of the gameBoard
     - [ ] Check if null else return
 - [ ] Build logic that checks winner
-- Clean up interface:
+- [ ] Clean up interface:
     - [ ] Allow players to add names
     - [ ] Start/Restart Button
 
--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const gameBoard = (function () {
-  let board = [];
-})();
+// const displayController = (function () {
+//   let turn = player1Turn ? player1Action() : player2Action();
+// })();
 
-const displayController = (function () {})();
+// const playerFactory = (name, shape) => {
+//   return { name, shape };
+// };
 
-const playerFactory = (name, shape) => {
-  return { name, shape };
-};
+const TicTacToeBoard = (function () {
+  function create(boardSize, callback) {
+    const board = Array.from({length: boardSize}, () => new Array(boardSize).fill(null));
+    const boardElem = document.querySelector('.board');
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell', 'material-symbols-outlined')
+        cell.addEventListener('click', () => {
+          callback(i, j);
+          if (cell.innerHTML === '') {
+            cell.innerHTML = 'close'
+          } else if (cell.innerHTML === 'close') {
+            cell.innerHTML = 'circle'
+          } else if (cell.innerHTML === 'circle') {
+            cell.innerHTML = ''
+          }
+        })
+        boardElem.appendChild(cell)
+        board[i][j] = cell
+      }
+    }
+    return board
+  }
+  return {
+    create,
+  }
+})()
+
+const board = TicTacToeBoard.create(3,(row, col) => {
+  console.log(`Cell clicked! Row ${row} on Col ${col}`)
+  console.log(board)
+})
