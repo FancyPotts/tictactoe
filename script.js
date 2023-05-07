6a4079fc29aa38b111d20b5a3d64d4ecf00473d5
pottsaf <aaronfpotts@gmail.com>
2023-05-06 21:38:21 -0600

    Make gameflowcontroller function display properly

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 
-
-// const playerFactory = (name, shape) => {
-//   return { name, shape };
-// };
-
 const TicTacToeBoard = (function () {
   function create(boardSize, callback) {
     const board = Array.from({length: boardSize}, () => new Array(boardSize).fill(null));
-    const boardElem = document.querySelector('.board');
+    const boardElem = document.querySelector('.board')
     for (let i = 0; i < boardSize; i++) {
       for (let j = 0; j < boardSize; j++) {
-        const cell = document.createElement('div');
+        const cell = document.createElement('div')
         cell.classList.add('cell', 'material-symbols-outlined', 'md-lg')
         cell.addEventListener('click', () => {
           callback(i, j);
-          displayController.turn();
-          if (cell.innerHTML === '') {
+          GameFlowController.turn()
+          if (turnCheck === true) {
             cell.innerHTML = 'close'
-          } else if (cell.innerHTML === 'close') {
+          } else if (turnCheck === false) {
             cell.innerHTML = 'circle'
-          } else if (cell.innerHTML === 'circle') {
-            cell.innerHTML = ''
           }
         })
         boardElem.appendChild(cell)
@@ -30,20 +23,35 @@ const TicTacToeBoard = (function () {
     return board
   }
   return {
-    create,
+    create
   }
 })()
 
-const displayController = (function () {
-  let isPlayer1Turn = true;
+const GameFlowController = (function () {
+  let isPlayer1Turn = true
+  const displayTurn = document.getElementById('turn-display')
+
   function turn () {
     isPlayer1Turn = isPlayer1Turn ? false : true;
-    console.log(isPlayer1Turn)
+    turnCheck = isPlayer1Turn
+    display()
+    return turnCheck
   }
-  return { turn, turnCheck }
-})();
+  function display () {
+    if (isPlayer1Turn === true) {
+      displayTurn.innerHTML = 'Player 1'
+    } else {
+      displayTurn.innerHTML = 'Player 2'
+    }
+  }
+  return { turn }
+})()
 
 const board = TicTacToeBoard.create(3,(row, col) => {
   console.log(`Cell clicked! Row ${row} on Col ${col}`)
-  console.log(board) 
+  console.log(board)
 })
+
+// const playerFactory = (name, shape) => {
+//   return { name, shape };
+// }
